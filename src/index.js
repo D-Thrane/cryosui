9c37efe3a7e6dbad1f4e914c070f82f5c858d55b
Dennis Thrane <dwt@cryosinternational.com>
2019-03-13 11:45:13 +0100

    Init

---
+++ src/index.js
@@ -0,0 +1,9 @@
+/*** src/index.js   ***/
+
+import React from 'react';
+import './styles.css';
+
+const MyComponent = () => (
+    <h1>Hello from My CryosUI Component</h1>
+);
+export default MyComponent;
